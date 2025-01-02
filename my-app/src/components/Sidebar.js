68426a32308a92851b89a1bf4c3407fb68c8f119
Charles Enright <c.enright@me.com>
2025-01-02 12:43:41 +0000

    Add functional component for sidebar

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// function FunctionalGreeting() {
+//     return <h1>Hello from React!</h1>
+// }
+
+const Sidebar = () => (
+    <div>
+        <a href="#." target="_blank">My Photos</a>
+        <a href="#." target="_blank">My Illustrations</a>
+        <a href="#." target="_blank">My Paintings</a>
+    </div>
+);
+
+export default Sidebar;
