ed08955407b974646cbe1bc34560464ccf1d962c
Charles Enright <c.enright@me.com>
2025-01-02 12:52:02 +0000

    Add CSS to Sidebar component

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import css from "./css/Sidebar.module.css";
 
 // function FunctionalGreeting() {
 //     return <h1>Hello from React!</h1>
 // }
 
 const Sidebar = () => (
-    <div>
+    <div className={css.sidebar}>
         <a href="#." target="_blank">My Photos</a>
         <a href="#." target="_blank">My Illustrations</a>
         <a href="#." target="_blank">My Paintings</a>
